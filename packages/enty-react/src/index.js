ca6bf5986c311fdacd10219552cc8eb27b5318d7
Allan Hortle <allanhortle@gmail.com>
2018-03-02 14:02:00 +1100

    Export enty schemas from enty react

--- packages/enty-react/src/index.js
+++ packages/enty-react/src/index.js
@@ -1,5 +1,16 @@
 // @flow
 
+
+// Re-export enty core.
+export {default as ArraySchema} from 'enty/lib/ArraySchema';
+export {default as CompositeEntitySchema} from 'enty/lib/CompositeEntitySchema';
+export {default as DynamicSchema} from 'enty/lib/DynamicSchema';
+export {default as EntitySchema} from 'enty/lib/EntitySchema';
+export {default as ListSchema} from 'enty/lib/ListSchema';
+export {default as MapSchema} from 'enty/lib/MapSchema';
+export {default as ObjectSchema} from 'enty/lib/ObjectSchema';
+export {default as ValueSchema} from 'enty/lib/ValueSchema';
+
 // Api
 export {default as EntityApi} from './EntityApi';
 
