c6f2eb8ec70ff32e9b5a19e8d2a8925d7981854e
Allan Hortle <allanhortle@gmail.com>
2018-03-22 13:28:04 +1100

    BREAKING CHANGE: dont reconstruct entities
    Previously the merged entities were being reconstructed
    before being stored.
    
    Now they are constructed once and then either merged and stored
    or just stored straight away.

--- packages/enty/src/EntitySchema.js
+++ packages/enty/src/EntitySchema.js
@@ -76,12 +76,12 @@ export class EntitySchema extends Child implements Schema<Entity> {
         // list this schema as one that has been used
         schemas[name] = this;
 
+        result = definition.options.constructor(result);
 
-        if(previousEntity) {
-            result = definition.options.merge(previousEntity, result);
-        }
-
-        entities[name][id] = definition.options.constructor(result);
+        entities[name][id] = previousEntity
+            ? definition.options.merge(previousEntity, result)
+            : result
+        ;
 
         return {
             entities,
