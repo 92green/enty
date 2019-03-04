ee3a958016f7e74e8f517e8c343d76fafde47f5d
Allan Hortle <allanhortle@gmail.com>
2019-03-04 12:05:50 +1100

    amend: fix function signature

--- packages/react-enty/src/MultiQueryHockFactory.js
+++ packages/react-enty/src/MultiQueryHockFactory.js
@@ -12,12 +12,9 @@ import Deprecated from './util/Deprecated';
  */
 function MultiQueryHockFactory(sideEffectList: Array<SideEffect>, hockOptions?: HockOptionsInput): Function {
     const actionPrefix = 'ENTITY';
-    const FETCH = `${actionPrefix}_FETCH`;
-    const RECEIVE = `${actionPrefix}_RECEIVE`;
-    const ERROR = `${actionPrefix}_ERROR`;
 
     Deprecated('MultiQueryHockFactory has been deprecated in favor of much improved MultiRequestHock. Check the docs for usage instructions.');
-    return EntityQueryHockFactory(createAllRequestAction(FETCH, RECEIVE, ERROR, sideEffectList), hockOptions);
+    return EntityQueryHockFactory(createAllRequestAction(actionPrefix, sideEffectList), hockOptions);
 }
 
 export default MultiQueryHockFactory;
