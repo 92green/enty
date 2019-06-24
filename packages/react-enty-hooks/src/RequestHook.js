1bb2e6c9fb7e2e49c6b57498823457bf8c89bd16
Allan Hortle <allanhortle@gmail.com>
2019-06-24 14:05:53 +1000

    add: RequestHook

---
+++ packages/react-enty-hooks/src/RequestHook.js
@@ -0,0 +1,48 @@
+// @flow
+import Message from 'enty-state/lib/data/Message';
+import {EmptyState} from 'enty-state/lib/data/RequestState';
+import {useState, useContext, useCallback, useMemo, useRef} from 'react';
+
+type RequestHookConfig = {
+    actionType: string,
+    requestAction: Function,
+    generateResultKey: Function
+};
+
+export default function RequestHookFactory(context: *, config: RequestHookConfig) {
+    const {requestAction, generateResultKey} = config;
+
+    return () => {
+        const [resultKey, setResultKey] = useState();
+        const [state, dispatch] = useContext(context);
+        const responseRef = useRef();
+
+        let requestState = state._requestState[resultKey] || EmptyState();
+
+        let response = useMemo(() => {
+            return state._baseSchema.denormalize({
+                entities: state._entities,
+                result: state._result[resultKey]
+            });
+        }, [requestState, resultKey, state.normalizeCount]);
+
+        responseRef.current = response;
+
+        let onRequest = useCallback((payload) => {
+            const resultKey = generateResultKey(payload);
+            setResultKey(resultKey);
+            return dispatch(requestAction(payload, {resultKey}))
+                .then(() => responseRef.current)
+                .catch(() => {});
+        });
+
+
+        return useMemo(() => new Message({
+            resultKey,
+            requestState,
+            response,
+            requestError: state._error[resultKey],
+            onRequest
+        }), [requestState, response]);
+    };
+}
