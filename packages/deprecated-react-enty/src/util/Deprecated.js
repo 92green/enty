719837cec49c459838cb0e907bc64ad3b98dff3c
Allan Hortle <allanhortle@gmail.com>
2019-07-15 08:43:11 +1000

    build: remove old deprecated-react-enty

--- packages/deprecated-react-enty/src/util/Deprecated.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-export default function Deprecated(message: string) {
-    if(process.env.NODE_ENV !== 'production') {
-        console.warn(`[Enty] DEPRECATED: ${message}`);
-    }
-}
-
