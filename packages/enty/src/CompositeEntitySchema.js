b5140b11de56f5252b4206e2f428344a770a205e
Allan Hortle <allanhortle@gmail.com>
2019-07-30 08:11:04 +1000

    BREAK: make CompositeEntitySchema extend EntitySchema
    It no longer makes a new type that merges two entitites.
    You declare the base type as it exists and declare the tainted keys
    through options.compoisiteKeys

--- packages/enty/src/CompositeEntitySchema.js
+++ packages/enty/src/CompositeEntitySchema.js
@@ -2,15 +2,9 @@
 import type {NormalizeState} from './util/definitions';
 import type {DenormalizeState} from './util/definitions';
 import type {EntitySchemaOptions} from './util/definitions';
-import type {EntitySchemaInterface} from './util/definitions';
-import type {StructuralSchemaInterface} from './util/definitions';
-import type {IdAttribute} from './util/definitions';
-import type {KeyedShape} from './util/definitions';
 
-import getIn from 'unmutable/lib/getIn';
-import {CompositeShapeMustBeEntityError} from './util/Error';
+import map from 'unmutable/lib/map';
 import {CompositeKeysMustBeEntitiesError} from './util/Error';
-import {NoShapeError} from './util/Error';
 import EntitySchema from './EntitySchema';
 
 function isEntitySchema(schema) {
@@ -18,72 +12,61 @@ function isEntitySchema(schema) {
 }
 
 
-export default class CompositeEntitySchema implements EntitySchemaInterface {
-    name: string;
+export default class CompositeEntitySchema extends EntitySchema {
     compositeKeys: Object;
-    shape: ?StructuralSchemaInterface;
-    idAttribute: IdAttribute;
 
     constructor(
         name: string,
-        options: EntitySchemaOptions & {compositeKeys: KeyedShape} = {}
+        options: EntitySchemaOptions & {compositeKeys: Object} = {}
     ) {
-        this.name = name;
-        this.shape = options.shape;
+        super(name, options);
         this.compositeKeys = options.compositeKeys;
-        this.idAttribute = options.idAttribute || (() => {});
     }
 
     normalize(data: Object, entities?: Object = {}): NormalizeState {
-        const {shape, compositeKeys, name} = this;
+        const {compositeKeys, name} = this;
         const adjustedData = Object.assign({}, data);
 
         let idList = [];
 
-        if(!shape) {
-            throw NoShapeError(name);
-        }
-
-        if(!isEntitySchema(shape)) {
-            throw CompositeShapeMustBeEntityError(name, shape.constructor.name);
-        }
-
-        const compositeResults = Object.keys(this.compositeKeys)
-            .reduce((rr: Object, key: string): Object => {
+        const compositeResults = map(
+            (schema, key) => {
                 if(!isEntitySchema(compositeKeys[key])) {
                     throw CompositeKeysMustBeEntitiesError(`${name}.${key}`, compositeKeys[key].constructor.name);
                 }
-
-                // Check that there is data before be continue
-                // normalizing compositeKeys
-                if(!adjustedData[key]) {
-                    rr[key] = null;
-                    return rr;
+                if(adjustedData[key] == null) {
+                    return null;
                 }
 
+                // normalize the tainted key
                 const {result: compositeResult} = compositeKeys[key].normalize(adjustedData[key], entities);
 
-                rr[key] = compositeResult;
-                idList.push(compositeResult);
+                // remove tainted taineted key from main entity
                 delete adjustedData[key];
-                return rr;
-            }, Object.assign({}, this.compositeKeys));
 
+                // store its id
+                idList.push(compositeResult);
+
+                return compositeResult;
+
+            }
+        )(compositeKeys);
 
 
         // recurse into the main shape
-        let {schemas, result: mainResult} = shape.normalize(adjustedData, entities);
+        let {schemas, result: mainResult} = super.normalize(adjustedData, entities);
 
-        // $FlowFixMe - Error is so obtuse its not googlable: string [1] is not an object.
         const result = {
-            [shape.name]: mainResult,
+            [name]: mainResult,
             ...compositeResults
         };
+
 
         const id = [mainResult]
             .concat(idList)
             .join('-')
         ;
+
 
         entities[name] = entities[name] || {};
         entities[name][id] = result;
@@ -91,6 +74,7 @@ export default class CompositeEntitySchema implements EntitySchemaInterface {
 
         // Save the schema
         schemas[name] = this;
+
 
         return {
             entities,
@@ -104,27 +88,22 @@ export default class CompositeEntitySchema implements EntitySchemaInterface {
      */
     denormalize(denormalizeState: DenormalizeState, path?: Array<*> = []): any {
         const {shape, compositeKeys, name} = this;
-        const {result, entities} = denormalizeState;
+        const {entities} = denormalizeState;
 
-        const entity = getIn([name, result])(entities);
+        // turn the composite id into its main and extra keys
+        const result = super.denormalize(denormalizeState, path);
 
-        const mainDenormalizedState = shape.denormalize({result: entity[shape.name], entities}, path);
+        const mainDenormalizedState = super.denormalize({result: result[name], entities}, path);
 
 
+        const compositeDenormalizedState = map(
+            (schema, key) => schema.denormalize({result: result[key], entities}, path)
+        )(compositeKeys);
 
-        const compositeDenormalizedState = Object.keys(compositeKeys)
-            .reduce((rr: Object, key: string): Object => {
-                // Check that there is data before be continue
-                // denormalize compositeKeys
-                if(!entity[key]) {
-                    rr[key] = null;
-                    return rr;
-                }
-                rr[key] = compositeKeys[key].denormalize({result: entity[key], entities}, path);
-                return rr;
-            }, Object.assign({}, this.compositeKeys));
-
-        return shape.shape.merge(mainDenormalizedState, compositeDenormalizedState);
+        return shape.merge(
+            mainDenormalizedState,
+            compositeDenormalizedState
+        );
     }
 }
 
--- packages/enty/src/__tests__/CompositeEntitySchema-test.js
+++ packages/enty/src/__tests__/CompositeEntitySchema-test.js
@@ -4,24 +4,19 @@ import CompositeEntitySchema from '../CompositeEntitySchema';
 import ObjectSchema from '../ObjectSchema';
 
 var course = new EntitySchema('course');
-var dog = new EntitySchema('dog');
-var participant = new EntitySchema('participant');
-
 course.shape = new ObjectSchema({});
-dog.shape = new ObjectSchema({});
-participant.shape = new ObjectSchema({});
 
-var courseParticipant = new CompositeEntitySchema('courseParticipant', {
-    shape: participant,
+var participant = new CompositeEntitySchema('participant', {
+    shape: new ObjectSchema({}),
     compositeKeys: {
         course
     }
 });
 
-var dogCourseParticipant = new CompositeEntitySchema('dogCourseParticipant', {
-    shape: dog,
+var dog = new CompositeEntitySchema('dog', {
+    shape: new ObjectSchema({}),
     compositeKeys: {
-        courseParticipant
+        participant
     }
 });
 
@@ -38,51 +33,20 @@ const derek = {
 
 
 test('denormalize is the inverse of normalize', () => {
-    expect(courseParticipant.denormalize(courseParticipant.normalize(derek))).toEqual(derek);
-});
-
-test('cannot be made up of structural types', () => {
-    const badDefinition = new CompositeEntitySchema('badDefinition', {
-        shape: new ObjectSchema({course})
-    });
-
-    const badKeys = new CompositeEntitySchema('badKeys', {
-        shape: course,
-        compositeKeys: {
-            deep: new ObjectSchema({participant})
-        }
-    });
-
-    const data = {
-        id: '123',
-        deep: {
-            participant: {
-                id: 'spk456',
-                courseName: 'Learn All The Things!'
-            }
-        }
-    };
-
-    expect(() => badKeys.normalize(data)).toThrow();
-    expect(() => badDefinition.normalize(data)).toThrow();
-});
-
-test('options.idAttribute defaults to an empty function', () => {
-    const {idAttribute} = new CompositeEntitySchema('foo');
-    expect(idAttribute()).toBe(undefined);
+    expect(participant.denormalize(participant.normalize(derek))).toEqual(derek);
 });
 
 test('throw without a definition', () => {
     expect(() => new CompositeEntitySchema('foo').normalize(derek, {})).toThrow();
-    expect(() => new CompositeEntitySchema('foo', {definition: null}).normalize(derek, {})).toThrow();
+    // $FlowFixMe - deliberate missuse of types
+    expect(() => new CompositeEntitySchema('foo', {shape: null}).normalize(derek, {})).toThrow();
 });
-
 
 test('can hold CompositeEntitySchemas', () => {
 
     const aa = {
         id: 'lassie',
-        courseParticipant: {
+        participant: {
             id: '123',
             name: 'Derek Tibbs',
             course: {
@@ -92,45 +56,37 @@ test('can hold CompositeEntitySchemas', () => {
         }
     };
 
-    const {entities, result} = dogCourseParticipant.normalize(aa);
+    const {entities, result} = dog.normalize(aa);
     expect(entities.course).toBeTruthy();
-    expect(entities.courseParticipant).toBeTruthy();
+    expect(entities.participant).toBeTruthy();
     expect(entities.dog).toBeTruthy();
-    expect(entities.dogCourseParticipant).toBeTruthy();
+    expect(entities.dog).toBeTruthy();
     expect(entities.participant).toBeTruthy();
     expect(result).toBe('lassie-123-spk456');
 });
 
 test('can defer their definition', () => {
-    var lateCourseParticipant = new CompositeEntitySchema('courseParticipant', {
-        compositeKeys: {
-            course
-        }
-    });
-
-    lateCourseParticipant.shape = participant;
-
+    var lateCourseParticipant = new CompositeEntitySchema('participant');
+    lateCourseParticipant.shape = new ObjectSchema({});
+    lateCourseParticipant.compositeKeys = {course};
     expect(() => lateCourseParticipant.normalize(derek)).not.toThrow();
 });
 
 
 test('compositeKeys will override defintion keys ', () => {
-    const cat = new EntitySchema('cat', {
-        shape: new ObjectSchema({friend: dog})
-    });
 
     const owl = new EntitySchema('owl', {
         shape: new ObjectSchema({})
     });
 
-    var catOwl = new CompositeEntitySchema('catOwl', {
-        shape: cat,
+    var cat = new CompositeEntitySchema('cat', {
+        shape: new ObjectSchema({friend: dog}),
         compositeKeys: {
             friend: owl
         }
     });
 
-    const {entities} = catOwl.normalize({
+    const {entities} = cat.normalize({
         id: 'sparky',
         friend: {
             id: 'hedwig'
@@ -138,17 +94,17 @@ test('compositeKeys will override defintion keys ', () => {
     });
 
 
-    expect(entities.catOwl['sparky-hedwig']).toBeTruthy();
+    expect(entities.cat['sparky-hedwig']).toBeTruthy();
     expect(entities.dog).toBeFalsy();
 });
 
 test('will not try to normalize null compositeKeys', () => {
-    expect(() => courseParticipant.normalize({id: 'rad'})).not.toThrow();
+    expect(() => participant.normalize({id: 'rad'})).not.toThrow();
 });
 
 test('will not try to denormalize null compositeKeys', () => {
     const data = {id: 'rad'};
-    expect(() => courseParticipant.denormalize(courseParticipant.normalize(data))).not.toThrow();
+    expect(() => participant.denormalize(participant.normalize(data))).not.toThrow();
 });
 
 
