b2295ea687a0d800f13f7da8b420a8f06af85615
Allan Hortle <allanhortle@gmail.com>
2019-09-03 08:32:19 +1000

    BREAK: make message polymorphic and less automatic
    You have to pass everything into the message all the time,
    but that gives you types on what is in the message. Message
    is mostly consumed by react-enty so its not too big a deal.

--- packages/enty-state/src/data/__tests__/Message-test.js
+++ packages/enty-state/src/data/__tests__/Message-test.js
@@ -2,6 +2,13 @@
 import Message from '../Message';
 import RequestState from '../../data/RequestState';
 
+const response = {name: 'foo'};
+const responseKey = 'FOO';
+const onRequest = async () => response;
+const requestError = 'ERROR!';
+
+const messageInput = {response, responseKey, onRequest, requestError};
+
 it('will let you set responseKey, response, requestState, requestError, onRequest', () => {
     const message = new Message({
         responseKey: 'foo',
@@ -20,12 +27,11 @@ it('will let you set responseKey, response, requestState, requestError, onReques
 
 
 it('will default requestState to Empty', () => {
-    expect(new Message().requestState.isEmpty).toBe(true);
+    expect(new Message({response: null, responseKey: 'foo', onRequest: Promise.resolve, requestError: null}).requestState.isEmpty).toBe(true);
 });
 
 it('will let you update the message', () => {
-    const newMessage = Message
-        .empty({response: 'foo'})
+    const newMessage = Message.empty(messageInput)
         .update(message => ({
             ...message,
             response: 'bar'
@@ -53,8 +59,8 @@ describe('Message response methods', () => {
     });
 
     it('will not break on empty responses', () => {
-        expect(Message.empty().get('blah', '!')).toBe('!');
-        expect(Message.empty().getIn(['bar', 'blah'], '!')).toBe('!');
+        expect(Message.empty(messageInput).get('blah', '!')).toBe('!');
+        expect(Message.empty(messageInput).getIn(['bar', 'blah'], '!')).toBe('!');
     });
 
 });
@@ -77,10 +83,10 @@ describe('Message requestState methods', () => {
 
     it('will change requestState with .to functions', () => {
         expect(Message.fetching().toEmpty().requestState.isEmpty).toBe(true);
-        expect(Message.empty().toFetching().requestState.isFetching).toBe(true);
-        expect(Message.empty().toRefetching().requestState.isRefetching).toBe(true);
-        expect(Message.empty().toSuccess().requestState.isSuccess).toBe(true);
-        expect(Message.empty().toError().requestState.isError).toBe(true);
+        expect(Message.empty(messageInput).toFetching().requestState.isFetching).toBe(true);
+        expect(Message.empty(messageInput).toRefetching().requestState.isRefetching).toBe(true);
+        expect(Message.empty(messageInput).toSuccess().requestState.isSuccess).toBe(true);
+        expect(Message.empty(messageInput).toError().requestState.isError).toBe(true);
     });
 
 
@@ -88,38 +94,38 @@ describe('Message requestState methods', () => {
 
 describe('Message Constructors', () => {
     test('Message.empty will create a empty message without a response', () => {
-        const message = Message.empty({responseKey: 'bar'});
+        const message = Message.empty(messageInput);
         expect(message.response).toBeUndefined();
         expect(message.requestState.isEmpty).toBe(true);
-        expect(message.responseKey).toBe('bar');
+        expect(message.responseKey).toBe('FOO');
     });
 
     test('Message.fetching will create a fetching message without a response', () => {
-        const message = Message.fetching({responseKey: 'bar'});
+        const message = Message.fetching(messageInput);
         expect(message.response).toBeUndefined();
         expect(message.requestState.isFetching).toBe(true);
-        expect(message.responseKey).toBe('bar');
+        expect(message.responseKey).toBe('FOO');
     });
 
     test('Message.refetching will create a refetching message with a response', () => {
-        const message = Message.refetching('foo', {responseKey: 'bar'});
+        const message = Message.refetching('foo', messageInput);
         expect(message.response).toBe('foo');
         expect(message.requestState.isRefetching).toBe(true);
-        expect(message.responseKey).toBe('bar');
+        expect(message.responseKey).toBe('FOO');
     });
 
     test('Message.success will create a success message with a response', () => {
-        const message = Message.success('foo', {responseKey: 'bar'});
+        const message = Message.success('foo', messageInput);
         expect(message.response).toBe('foo');
         expect(message.requestState.isSuccess).toBe(true);
-        expect(message.responseKey).toBe('bar');
+        expect(message.responseKey).toBe('FOO');
     });
 
     test('Message.error will create a error message with requestError', () => {
-        const message = Message.error('foo', {responseKey: 'bar'});
+        const message = Message.error('foo', messageInput);
         expect(message.requestError).toBe('foo');
         expect(message.requestState.isError).toBe(true);
-        expect(message.responseKey).toBe('bar');
+        expect(message.responseKey).toBe('FOO');
     });
 
     it('will not break if nothing is passed to each constructor', () => {
