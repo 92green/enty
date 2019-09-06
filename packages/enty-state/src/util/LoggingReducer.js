e2afe36d3da125fd53b07be596fc7a1305685a34
Allan Hortle <allanhortle@gmail.com>
2019-09-06 18:10:32 +1000

    amend: add final detail to LoggingReducer

--- packages/enty-state/src/util/LoggingReducer.js
+++ packages/enty-state/src/util/LoggingReducer.js
@@ -5,13 +5,44 @@ const log = (first: string, ...rest: mixed[]) => console.log(`%c${first}`, css, 
 const group = (name: string) => console.group(`%c[${name}]`, css);
 const groupEnd = (name: string) => console.groupEnd(`%c[${name}]`, css);
 
-export default function LoggingReducer(state, action) {
-    const {type, meta, payload} = action
-    group(type);
-    log('Meta', meta);
-    log('Payload', payload);
-    log('Next State', state);
-    groupEnd(type);
+export default function LoggingReducer(state, action, debugName) {
+    const {type, meta = {}, payload} = action;
+    const groupName = `${debugName}: ${type}`;
+
+    group(groupName);
+
+    const {responseKey} = meta;
+    const requestState = state.requestState[responseKey]
+    const requestStateType = requestState && requestState
+        .emptyMap(() => 'empty')
+        .fetchingMap(() => 'fetching')
+        .refetchingMap(() => 'refetching')
+        .errorMap(() => 'error')
+        .successMap(() => 'success')
+        .value();
+
+    if(type === 'ENTY_INIT') {
+    }
+    else if (type === 'ENTY_FETCH') {
+        log('responseKey:', responseKey, requestStateType);
+    }
+    else if (type === 'ENTY_RECEIVE') {
+        log('responseKey:', responseKey, requestStateType);
+        log('stats.responseCount', state.stats.responseCount);
+        log('payload:', payload);
+        log('response:', state.response[responseKey]);
+        log('entities:', state.entities);
+    }
+    else if (type === 'ENTY_ERROR') {
+        log('responseKey:', responseKey, requestStateType);
+        log('error:', state.error[responseKey]);
+    }
+    else if (type === 'ENTY_REMOVE') {
+        log('stats.responseCount', state.stats.responseCount);
+        log('remove:', payload.join('.'));
+        log(`entities.${payload[0]}:`, state.entities[payload[0]]);
+    }
+    groupEnd(groupName);
     return state;
 }
 
