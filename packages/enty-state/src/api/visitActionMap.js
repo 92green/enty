eb3a3e00f9f8555004415aab32526184b913a1c7
Damien Clarke <dxinteractive@gmail.com>
2019-10-31 13:07:09 +1100

    fix: apis were created with recursive duplication of all object branches
    the entire reduction was being passed into each child recursion and being used as the starting point
    of each child's reduction, making circular object references and appending each child's keys onto a single object.
    
    this fixes the issue by using an empty object as the starting point of each new child reduction, which works because
    each recursed element explicitly builds all of its own keys onto its own output object.

--- packages/enty-state/src/api/visitActionMap.js
+++ packages/enty-state/src/api/visitActionMap.js
@@ -7,19 +7,19 @@ import pipeWith from 'unmutable/lib/util/pipeWith';
 // Recurse through deep objects and apply the visitor to
 // anything that isnt another object.
 //
-export default function visitActionMap(branch: *, visitor: Function, path: string[] = [], state: * = {}): * {
+export default function visitActionMap(branch: *, visitor: Function, path: string[] = []): * {
     return pipeWith(
         branch,
         reduce(
             (reduction: *, item: *, key: string): * => {
                 if(typeof item !== 'function' && isKeyed(item)) {
-                    reduction[key] = visitActionMap(item, visitor, path.concat(key), reduction);
+                    reduction[key] = visitActionMap(item, visitor, path.concat(key));
                 } else {
                     reduction[key] = visitor(item, path.concat(key));
                 }
                 return reduction;
             },
-            state
+            {}
         ),
     );
 }
