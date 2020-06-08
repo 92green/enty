819e076dce349ec3b25a4023ed741b3113a1ea26
Allan Hortle <allanhortle@gmail.com>
2020-06-08 11:19:03 +1000

    Use class based provider to subscriptions can be the first thing

--- packages/react-enty/src/Provider.tsx
+++ packages/react-enty/src/Provider.tsx
@@ -1,25 +1,32 @@
-import React, {useContext, useEffect, useState, FunctionComponent, Context} from 'react';
+import React, {Component, ReactNode} from 'react';
 import {EntityStore} from 'enty-state';
-
-export type StoreContext = {};
-export const StoreContext: Context<StoreContext> = React.createContext({});
-export const useStore = () => useContext(StoreContext);
+import {StoreContext} from './StoreContext';
 
 type Props = {
-    children: Element;
+    children: ReactNode;
     debug?: string;
     store: EntityStore<any>;
 };
 
-export const Provider: FunctionComponent<Props> = ({children, debug, store}) => {
-    const [value, setState] = useState({store});
-
-    useEffect(() => {
-        store.subscribe(() => {
-            if (debug) console.log(store);
-            setState({store});
-        });
-    }, []);
-
-    return <StoreContext.Provider children={children} value={value} />;
+type State = {
+    count: number;
+    store: EntityStore<any>;
 };
+
+export default class Provider extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        props.store.subscribe(this.updateStore.bind(this));
+        this.state = {
+            count: 0,
+            store: props.store
+        };
+    }
+    updateStore(store: EntityStore<any>) {
+        if (this.props.debug) console.log(store);
+        this.setState({store, count: store.normalizeCount});
+    }
+    render() {
+        return <StoreContext.Provider children={this.props.children} value={this.state} />;
+    }
+}
--- packages/react-enty/src/__tests__/ProviderFactory-test.tsx
+++ packages/react-enty/src/__tests__/ProviderFactory-test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {mount} from 'enzyme';
-import ProviderFactory from '../ProviderFactory';
+import ProviderFactory from '../Provider';
 import composeWith from 'unmutable/composeWith';
 
 const expectContext = (testFn) => {
@@ -13,78 +13,44 @@ const expectContext = (testFn) => {
     );
 };
 
-describe('Factory', () => {
-    it('returns a hock, component and context', () => {
-        expect(ProviderFactory({reducer: (aa) => aa})).toMatchObject({
-            Provider: expect.any(Function),
-            ProviderHoc: expect.any(Function),
-            Context: expect.any(Object)
-        });
-    });
-});
+//describe('Hoc', () => {
+//it('returns a config => component => props function', () => {
+//const {ProviderHoc, Context} = ProviderFactory({});
 
-describe('Component', () => {
-    it('applies the intial state to a context', () => {
-        const {Provider, Context} = ProviderFactory({});
+//expectContext((Child) =>
+//composeWith(
+//(Component) => (props) => (
+//<Component {...props} initialState={{entities: {foo: 'hoc'}}} />
+//),
+//ProviderHoc(),
+//() => {
+//return <Context.Consumer children={(context) => <Child context={context} />} />;
+//}
+//)
+//).toMatchObject([
+//{
+//baseSchema: undefined,
+//entities: {foo: 'hoc'},
+//request: {},
+//schemas: {},
+//stats: {responseCount: 0}
+//},
+//expect.any(Function)
+//]);
+//});
+//});
 
-        expectContext((Child) => () => {
-            return (
-                <Provider initialState={{entities: {foo: 'component'}}}>
-                    <Context.Consumer children={(context) => <Child context={context} />} />
-                </Provider>
-            );
-        }).toMatchObject([
-            {
-                baseSchema: undefined,
-                entities: {foo: 'component'},
-                request: {},
-                schemas: {},
-                stats: {responseCount: 0}
-            },
-            expect.any(Function)
-        ]);
-    });
-});
+//describe('Debugging', () => {
+//it('will log reducer state if debug prop is provided', () => {
+//const group = jest.spyOn(console, 'group').mockImplementation(() => {});
+//const groupEnd = jest.spyOn(console, 'groupEnd').mockImplementation(() => {});
+//const {Provider} = ProviderFactory({});
+//mount(<Provider debug="Foo" />);
 
-describe('Hoc', () => {
-    it('returns a config => component => props function', () => {
-        const {ProviderHoc, Context} = ProviderFactory({});
+//expect(group).toHaveBeenCalled();
+//expect(groupEnd).toHaveBeenCalled();
 
-        expectContext((Child) =>
-            composeWith(
-                (Component) => (props) => (
-                    <Component {...props} initialState={{entities: {foo: 'hoc'}}} />
-                ),
-                ProviderHoc(),
-                () => {
-                    return <Context.Consumer children={(context) => <Child context={context} />} />;
-                }
-            )
-        ).toMatchObject([
-            {
-                baseSchema: undefined,
-                entities: {foo: 'hoc'},
-                request: {},
-                schemas: {},
-                stats: {responseCount: 0}
-            },
-            expect.any(Function)
-        ]);
-    });
-});
-
-describe('Debugging', () => {
-    it('will log reducer state if debug prop is provided', () => {
-        const group = jest.spyOn(console, 'group').mockImplementation(() => {});
-        const groupEnd = jest.spyOn(console, 'groupEnd').mockImplementation(() => {});
-        const {Provider} = ProviderFactory({});
-        mount(<Provider debug="Foo" />);
-
-        expect(group).toHaveBeenCalled();
-        expect(groupEnd).toHaveBeenCalled();
-
-        group.mockRestore();
-        groupEnd.mockRestore();
-    });
-});
-
+//group.mockRestore();
+//groupEnd.mockRestore();
+//});
+//});
