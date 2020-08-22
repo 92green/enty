59e0897638d2977a97a3e9eca6d02c07ecc4dcfb
Allan Hortle <allanhortle@gmail.com>
2020-08-22 11:21:45 +1000

    Remove CompositeEntitySchema & DynamicSchema & IdSchema
    Probably will bring them back soon. But for v2 I want the api to be
    really slim

--- packages/react-enty/src/index.ts
+++ packages/react-enty/src/index.ts
@@ -8,8 +8,5 @@ export {default as useAutoRequest} from './useAutoRequest';
 
 // Schemas
 export {default as ArraySchema} from 'enty/lib/ArraySchema';
-export {default as CompositeEntitySchema} from 'enty/lib/CompositeEntitySchema';
-export {default as DynamicSchema} from 'enty/lib/DynamicSchema';
 export {default as EntitySchema} from 'enty/lib/EntitySchema';
-export {default as IdSchema} from 'enty/lib/IdSchema';
 export {default as ObjectSchema} from 'enty/lib/ObjectSchema';
