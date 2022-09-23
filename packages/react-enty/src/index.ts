b3f59b06acc7c52db01439dd90fd30c1d39476c4
Allan Hortle <allanhortle@gmail.com>
2022-09-23 20:03:09 +1000

    Remove deprecated helper exports

--- packages/react-enty/src/index.ts
+++ packages/react-enty/src/index.ts
@@ -1,7 +1,5 @@
 // Api
 export {default as EntityApi} from './EntityApi';
-export {default as LoadingBoundary} from './LoadingBoundary';
-export {default as LoadingBoundaryHoc} from './LoadingBoundaryHoc';
 export {default as RequestState} from './data/RequestState';
 export {default as Message} from './data/Message';
 export {MessageFactory} from './data/Message';
@@ -10,12 +8,8 @@ export {FetchingMessage} from './data/Message';
 export {RefetchingMessage} from './data/Message';
 export {SuccessMessage} from './data/Message';
 export {ErrorMessage} from './data/Message';
-export {default as useAutoRequest} from './useAutoRequest';
 
 // Schemas
 export {ArraySchema} from 'enty';
-export {CompositeEntitySchema} from 'enty';
-export {DynamicSchema} from 'enty';
 export {EntitySchema} from 'enty';
-export {IdSchema} from 'enty';
 export {ObjectSchema} from 'enty';
--- packages/react-enty/src/useAutoRequest.ts
+++
@@ -1,7 +0,0 @@
-import {useEffect} from 'react';
-
-export default function useAutoRequest(fn: Function, deps: Array<unknown> = []) {
-    return useEffect(() => {
-        fn();
-    }, deps);
-}
